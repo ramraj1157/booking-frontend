1cfe76b2ceb8b229f726e59f9da3f777efb8edeb
ramrajb1096 <rrnayak2004@gmail.com>
2025-05-02 21:46:37 +0530

    frontend

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,7 +1,5 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import Hero from "../components/Hero";
-import SearchBar from "../components/SearchBar";
 
 interface Props {
   children: React.ReactNode;
