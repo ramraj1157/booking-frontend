f923e846add7e2a3d43e1ff1446c36377670ec46
ramrajb1096 <rrnayak2004@gmail.com>
2025-05-02 21:40:14 +0530

    frontend

--- src/api-client.ts
+++ src/api-client.ts
@@ -5,7 +5,7 @@ import {
   HotelType,
   PaymentIntentResponse,
   UserType,
-} from "../../backend/src/shared/types";
+} from "./shared/types";
 import { BookingFormData } from "./forms/BookingForm/BookingForm";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
